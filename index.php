f2a348dfba4a28a16d904427ff12b768c80c12df
Bryce Sharp <bryce@gorocketfuel.com>
2018-12-31 10:21:25 -0600

    Don't commit your vendor

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require __DIR__ . '/vendor/autoload.php';
-
-    $data = file_get_contents ("src/example-graph.json");
-    $json = json_decode($data, true);
-
-    $graph = new Graph\Graph($json);
-    echo "v: " . $graph->countVertices();
-    echo PHP_EOL . "e:" . $graph->countEdges();
